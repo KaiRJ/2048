1cb62d56646a6ff395807795cc98b032809cc1fd
Kai PC <kairjenkins@gmail.com>
2025-02-23 22:33:42 +0000

    spawn square in random panel at start of game

--- main.gd
+++ main.gd
@@ -12,12 +12,18 @@ func add_square_to_grid():
 	var square = square_scene.instantiate()
 
 	# TODO: get random panel
-	var panel = $Grid/PanelContainer/MarginContainer/GridContainer/Panel5
+	var panel = get_random_panel()
 
 	# Layouts update at end of frame, so call update_square_position on next frame.
 	call_deferred("update_square_position", square, panel)
 
 	$Grid/Squares.add_child(square)
+
+
+func get_random_panel():
+	var grid_panels = get_tree().get_nodes_in_group("GridPanels")
+	var panel_index = randi() % len(grid_panels)
+	return grid_panels[panel_index]
 
 
 func update_square_position(square, panel):
